ca723b14563d2120869e70f8bf5c20499a1e9379
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-08-27 16:24:58 -0400

    add /version endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
-'use strict'
+const { send } = require('micro')
+const { router, get, post } = require('microrouter')
+const versionMiddleware = require('version-middleware')()
 
+const hello = async (req, res) =>
+  send(res, 200, await Promise.resolve(`Hello ${req.params.who}`))
 
+const saveTests = async (req, res) => {
+  const { client, project, runId } = req.params
+  console.log(
+    'saving tests for client "%s" project "%s" run "%s"',
+    client,
+    project,
+    runId
+  )
+  send(res, 200)
+}
 
-module.exports = true
+const version = (req, res) => versionMiddleware(send.bind(null, res, 200))
 
+module.exports = router(
+  get('/hello/:who', hello),
+  post('/tests/:client/:project/:runId', saveTests),
+  get('/version', version)
+)
